fc258e124cc81d5fa73d0eef2cdc931f11c05606
Szpiss <3334705449@qq.com>
2026-03-31 06:48:52 +0000

    移除ROS编译生成目录并完善gitignore

--- build/service_demo/CMakeFiles/service_demo_generate_messages_nodejs.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/cuing/catkin_ws/devel/share/gennodejs/ros/service_demo/srv/AddTwoInts.js"
-  "CMakeFiles/service_demo_generate_messages_nodejs"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/service_demo_generate_messages_nodejs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/service_demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cuing/catkin_ws/src/service_demo
-# Build directory: /home/cuing/catkin_ws/build/service_demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
